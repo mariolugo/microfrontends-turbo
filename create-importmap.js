e81f8cfbfaabef9b102244cf0024407111d79346
Mario Lugo <mariolugo23@gmail.com>
2022-07-12 16:47:26 -0500

    add after deploy

---
+++ create-importmap.js
@@ -0,0 +1,12 @@
+// import fs from "fs";
+// import path from "path";
+
+const space = "@mftest";
+const apps = ["root-config", "app1", "app2"];
+
+const args = process.argv.slice(2);
+const createImportMap = (parameters) => {
+  console.log("params", parameters);
+};
+
+createImportMap(args);
