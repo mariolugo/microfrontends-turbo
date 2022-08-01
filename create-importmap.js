d0c0e02ee4e8b1fd90cc04cbef129c4552f49a31
Mario Lugo <mariolugo23@gmail.com>
2022-08-01 13:36:59 -0500

    move to public vercel

--- create-importmap.js
+++ create-importmap.js
@@ -35,13 +35,6 @@ function createImportMap(parameters) {
         if (err) console.log("error", err);
       }
     );
-    // fs.writeFile(
-    //   "./vercel/output/importmap.json",
-    //   dictstring,
-    //   function (err, result) {
-    //     if (err) console.log("error", err);
-    //   }
-    // );
   }
 
   createImportMapFile();
